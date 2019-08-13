1cea6285e89a452dceb0915b5926a511108740d8
Christopher LaPointe <cmlapointe11@gmail.com>
2019-08-13 18:18:56 -0400

    Config docs

--- config.js
+++ config.js
@@ -1,19 +1,28 @@
 module.exports = require('rc')('oidcproxy', {
   oidc: {
+    // Ubsub (or other) OIDC settings
     auth_url: 'https://app.ubsub.io/oidc',
     token_url: 'https://app.ubsub.io/oidc/token',
     client_id: null,
     client_secret: null,
-    scope: 'user',
+    // Where to request redirect back to
     redirect_base: 'http://localhost:8080/auth',
+    // What scopes to request
+    scope: 'user',
+    // Where to store the token
     store: 'cookie',
+    // If true, parse the JWT rather than passing directly
     parseStore: true,
   },
   backend: {
     port: 8080, // If proxying the request locally (as opposed to middleware), the port
+    // Path for the authentication endpoint
     path: '/auth',
+    // Static relative path to serve from
     static: null,
-    proxy: { // config to http-proxy-middleware (http-proxy)
+    // config to http-proxy-middleware (http-proxy)
+    // See: https://www.npmjs.com/package/http-proxy-middleware
+    proxy: {
       target: null,
       changeOrigin: true,
     },
