5779314b83413fca50b595d88804a88fce58e062
Christopher LaPointe <cmlapointe11@gmail.com>
2019-08-12 23:23:48 -0400

    More skeleton of code trading

--- index.js
+++ index.js
@@ -8,14 +8,34 @@ module.exports = function buildOidcProxy(opts = defaultConfig.oidc) {
 
   router.get('/', (req, res) => {
     const uri = URL.parse(opts.auth_url, true);
-    if (!uri.query)
+    if (!uri.query) {
       uri.query = {};
+    }
     uri.query.client_id = opts.client_id;
     uri.query.response_type = 'code';
     uri.query.scope = opts.scope;
     uri.query.redirect_uri = `${opts.redirect_base}/validate`;
     uri.query.state = uuid();
     res.redirect(URL.format(uri));
+  });
+
+  router.get('/validate', (req, res) => {
+    const state = req.query.state;
+    const code = req.query.code;
+    // TODO: Validate state
+    const uri = URL.parse(opts.token_url, true);
+    if (!uri.query) {
+      uri.query = {};
+    }
+    uri.query.client_id = opts.client_id;
+    uri.query.client_secret = opts.client_secret;
+    uri.query.grant_type = 'authorization_code';
+    uri.query.code = code;
+    axios.get(URL.format(uri))
+      .then(resp => {
+        // TODO: Validate JWT
+        res.send(resp.data);
+      });
   });
 
   return router;
